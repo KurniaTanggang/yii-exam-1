48e9c9e8035abd8b50c89f344b47619df0ba75f0
KurniaTanggang <kurniasitanggang1@gmail.com>
2023-02-13 08:54:35 +0700

    ubah biodata pada siswa

--- siswa/views/biodata/view.php
+++ siswa/views/biodata/view.php
@@ -6,20 +6,17 @@ use yii\widgets\DetailView;
 /* @var $model common\models\Siswa */
 ?>
 <div class="siswa-view">
-    <div class="table-responsive">
+  <div class="table-responsive">
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
             'nis',
             'nama',
             'tempat_lahir',
             'tanggal_lahir',
             'alamat:ntext',
-            'id_kelas',
-            'id_user',
         ],
     ]) ?>
-    </div>
+  </div>
 
 </div>
