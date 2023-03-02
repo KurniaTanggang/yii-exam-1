89740c0b03452278cdc10cb06fc45e460606bfb6
KurniaTanggang <kurniasitanggang1@gmail.com>
2023-03-02 14:02:33 +0700

    menambahkan chartJs pada dashbord

--- siswa/views/site/index.php
+++ siswa/views/site/index.php
@@ -1,8 +1,9 @@
 <?php
-
 /** @var yii\web\View $this */
+use dosamigos\chartjs\ChartJs;
 
 $this->title = 'My Yii Application';
+
 ?>
 <div class="site-index">
 
@@ -11,4 +12,37 @@ $this->title = 'My Yii Application';
 
   </div>
 
+  <?= ChartJs::widget([
+    'type' => 'line',
+    'options' => [
+        'height' => 200,
+        'width' => 400,
+    ],
+    'clientOptions' => [
+      'scales' => [
+          'yAxes' => [[
+              'ticks' => [
+                  'beginAtZero' => 'true', # Works
+              ]
+          ]],
+      ],
+  ], 
+    'data' => [
+        'labels' => ["X MIPA 1-SI", "X MIPA 1-SII", "XI MIPA 1-SI", "XI MIPA 1-SII", "XII MIPA 1-SI"],
+        'datasets' => [
+            [
+                'label' => "Perkembangan Akademik",
+                'backgroundColor' => "rgba(75, 192, 192,0.2)",
+                'borderColor' => "rgba(75, 192, 192,1)",
+                'pointBackgroundColor' => "rgba(75, 192, 192,1)",
+                'pointBorderColor' => "#2b34ed",
+                'pointHoverBackgroundColor' => "#2b34ed",
+                'pointHoverBorderColor' => "rgba(75, 192, 192,1)",
+                'data' => [73, 81, 90, 87, 92],
+                'tension' => "0.3"
+            ],
+        ]
+    ]
+]);
+?>
 </div>
